9404d064d188ccba64ae054d419076e087c16605
Babak <babakzarrinbal949@gmail.com>
2023-06-20 23:54:29 +0330

    refactoring logging added

--- back-end/src/main/java/com/demo/todo/service/TodoService.java
+++ back-end/src/main/java/com/demo/todo/service/TodoService.java
@@ -5,6 +5,8 @@ import com.demo.todo.repository.TodoRepository;
 import com.demo.todo.service.dto.TodoDTO;
 import com.demo.todo.service.mapper.TodoMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +15,12 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class TodoService {
+    private static final Logger logger = LoggerFactory.getLogger(TodoService.class);
     private final TodoRepository todoRepository;
     private final TodoMapper todoMapper;
 
     public List<TodoDTO> getAllTodos() {
+        logger.info("Getting all To-Dos");
         List<Todo> todos = todoRepository.findAll();
         return todos.stream()
                 .map(todoMapper::toDto)
@@ -24,18 +28,21 @@ public class TodoService {
     }
 
     public TodoDTO createTodo(TodoDTO todoDTO) {
+        logger.info("Creating a new To-Do");
         Todo todo = todoMapper.toEntity(todoDTO);
         Todo createdTodo = todoRepository.save(todo);
         return todoMapper.toDto(createdTodo);
     }
 
     public TodoDTO getTodoById(Long id) {
+        logger.info("Get Todo by id: {}", id);
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Todo not found"));
         return todoMapper.toDto(todo);
     }
 
     public TodoDTO updateTodoById(Long id, TodoDTO updatedTodoDTO) {
+        logger.info("Update Todo with id: {}", id);
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Todo not found"));
 
@@ -50,6 +57,7 @@ public class TodoService {
     }
 
     public void deleteTodoById(Long id) {
+        logger.info("Delete Todo with id: {}", id);
         todoRepository.deleteById(id);
     }
 }
